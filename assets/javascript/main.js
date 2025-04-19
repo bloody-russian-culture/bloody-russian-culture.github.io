9b3ba9f655a8ce72dc9fa856178cb0c9a577a9b5
bloody-russian-culture <bloody-russian-culture@users.noreply.github.com>
2025-04-19 23:43:11 +0000

    deploy: 66a8af97b5307d96690b89bf360ef54cd0418fcb

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,13 +1,21 @@
-function showSlides(folderName, slideIndex) {
-  let slides = document.getElementsByClassName(folderName + "-slides");
+function showSlides(container, folderName, slideIndex) {
+  let slides = container.getElementsByClassName(folderName + "-slides");
+
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length) { slideIndex = 1; }
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 1500, folderName, slideIndex);
+  setTimeout(showSlides, 1500, container, folderName, slideIndex);
 }
 
-showSlides("russian-culture", 0);
-showSlides("blood", 0);
+const comparisonContainers = document.querySelectorAll('.slideshow-wrapper');
+
+for (let i = 0; i < comparisonContainers.length; i++) {
+  const container = comparisonContainers[i]
+
+  showSlides(container, "left", 0);
+  showSlides(container, "right", 0);
+}
+
