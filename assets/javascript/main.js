b5b4cfdecd6e589d3f96405bbf970a26505d8a4e
bloody-russian-culture <bloody-russian-culture@users.noreply.github.com>
2025-05-06 01:04:03 +0000

    deploy: 716903292cae711cd5925a1c42c7fa4865c3d248

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -19,3 +19,15 @@ for (let i = 0; i < comparisonContainers.length; i++) {
   showSlides(container, "right", 0);
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll("a[href]")
+  const currentHost = location.hostname;
+
+  links.forEach(function(link) {
+    const linkHost = new URL(link.href).hostname
+    if (linkHost && linkHost !== currentHost) {
+      link.setAttribute("target", "_blank")
+      link.setAttribute("rel", "noopener noreferrer")
+    }
+  })
+})
